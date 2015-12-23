fca6a2fa59e23410db1654270374dac0e17f75ff
eslizn <eslizn@gmail.com>
2015-12-24 02:15:02 +0800

    fix

--- Provider.php
+++ Provider.php
@@ -19,11 +19,13 @@ class Provider extends ServiceProvider
 		
 		$this->app->make('Psr\Log\LoggerInterface')->setHandlers(array(new Logger()));
 		
+		$this->app['config']['cache.default'] = 'sae';
 		$this->app['config']['cache.stores.driver'] = 'sae';
 		$this->app->make('cache')->extend('sae', function($app){
 			return $app->make('cache')->repository(new Cache);
 		});
 		
+		$this->app['config']['session.default'] = 'sae';
 		$this->app['config']['session.stores.driver'] = 'sae';
 		$this->app->make('session')->extend('sae', function($app){
 			return new Session;
