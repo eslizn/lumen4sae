682731243f9a90bf26813057d854db202ac57e2a
eslizn <eslizn@gmail.com>
2015-12-24 03:21:31 +0800

    add mysql config

--- Provider.php
+++ Provider.php
@@ -21,14 +21,12 @@ class Provider extends ServiceProvider
 		$this->app->make('Psr\Log\LoggerInterface')->setHandlers(array(new Logger()));
 		
 		//config db
-		$this->app['config']['db.connection'] = 'mysql';
-		$this->app['config']['db.write.host'] = SAE_MYSQL_HOST_M;
-		$this->app['config']['db.read.host'] = SAE_MYSQL_HOST_S;
-		$this->app['config']['db.port'] = SAE_MYSQL_PORT;
-		$this->app['config']['db.database'] = SAE_MYSQL_DB;
-		$this->app['config']['db.username'] = SAE_MYSQL_USER;
-		$this->app['config']['db.password'] = SAE_MYSQL_PASS;
-		
+		$this->app['config']['database.connections.mysql.write.host'] = SAE_MYSQL_HOST_M;
+		$this->app['config']['database.connections.mysql.read.host'] = SAE_MYSQL_HOST_S;
+		$this->app['config']['database.connections.mysql.port'] = SAE_MYSQL_PORT;
+		$this->app['config']['database.connections.mysql.database'] = SAE_MYSQL_DB;
+		$this->app['config']['database.connections.mysql.username'] = SAE_MYSQL_USER;
+		$this->app['config']['database.connections.mysql.password'] = SAE_MYSQL_PASS;
 		
 		//config cache
 		/** @var \Illuminate\Cache\CacheManager $CacheManager **/
